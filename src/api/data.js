7806d7008ab3e009b30c777a167ae06cd4f478d8
Pawel Galwa <pawelgalwa.1@gmail.com>
2023-07-10 18:41:38 +0200

    Added data-mock

---
+++ src/api/data.js
@@ -0,0 +1,7 @@
+export const transactions = [
+    { customerId: 1, amount: 120, date: '2023-06-01' },
+    { customerId: 1, amount: 80, date: '2023-06-15' },
+    { customerId: 2, amount: 150, date: '2023-06-02' },
+    { customerId: 2, amount: 150, date: '2023-06-03' },
+    { customerId: 4, amount: 150, date: '2023-06-03' },
+  ];
